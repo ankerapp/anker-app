6689780d3184634b57cf0b3629040012acf03bb7
0xkhan <0xkhan@protonmail.com>
2023-03-21 22:29:03 +0100

    Add FreeFall plugin

---
+++ src/js/plugins/FreeFall.js
@@ -0,0 +1,90 @@
+export default class FreeFall {
+
+    constructor(config) {
+        this.dataAttributes = {
+            trigger: 'data-dropdown-trigger',
+            closer: 'data-dropdown-close',
+            id: 'data-dropdown-id'
+        };
+
+        this.options = {
+            showDropdownCSS: 'show-dd-container',
+            dataAttributes: this.dataAttributes
+        }
+
+        if (typeof config === 'object') {
+            this.options = { ...this.options, ...config };
+        }
+
+        this.triggerDataAtt = this.options.dataAttributes.trigger;
+        this.closerDataAtt = this.options.dataAttributes.closer;
+        this.ddDataAtt = this.options.dataAttributes.id;
+
+        this.currentDD = '';
+
+        this.init();
+    }
+
+    init() {
+        this.setTriggers();
+        this.setClosers();
+    }
+
+    /** Sets and adds event listeners to triggers */
+    setTriggers() {
+        const triggers = document.querySelectorAll(`[${this.triggerDataAtt}]`);
+
+        if (triggers.length > 0) {
+            triggers.forEach((trigger) => {
+                trigger.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    // ddID = dropdownID
+                    const ddID = event.target.getAttribute(`${this.triggerDataAtt}`);
+                    this.drop({ ddID: ddID });
+                });
+            });
+        }
+    }
+
+    /** Sets and adds event listeners to close buttons */
+    setClosers() {
+        const closeButtons = document.querySelectorAll(`[${this.closerDataAtt}]`);
+
+        if (closeButtons.length > 0) {
+            closeButtons.forEach((btn) => {
+                btn.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const ddID = event.target.getAttribute(`${this.closerDataAtt}`);
+                    this.roll({ ddID: ddID });
+                })
+            })
+        }
+    }
+
+    /**
+     * Opens dropdown container - drops it down
+     *
+     * @param {Object} Object.ddID The dropdown container ID
+     */
+    drop({ ddID }) {
+        const ddContainer = document.querySelector(`[${this.ddDataAtt}=${ddID}]`);
+
+        if (this.currentDD.length > 0) this.roll({ ddID: this.currentDD });
+
+        ddContainer.classList.add(this.options.showDropdownCSS);
+        this.currentDD = ddID;
+    }
+
+    /**
+     * Closes dropdown container - rolls it up
+     *
+     * @param {Object} Object.ddID The dropdown container ID
+     */
+    roll({ ddID }) {
+        const ddContainer = document.querySelector(`[${this.ddDataAtt}=${ddID}]`);
+
+        ddContainer.classList.remove(this.options.showDropdownCSS);
+        this.currentDD = '';
+    }
+
+}
