88a8015ca15a20de9d5dba6b20a1c0d7fcbd2f13
0xkhan <0xkhan@protonmail.com>
2023-03-23 00:48:44 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://ankerdata.netlify.app/data.json";
+export const API_URL = "https://ankerdata.netlify.app/dataPro.json";
 export const TIMEOUT_SEC = 10;
+export const THEME = "DefaultThemePro";
