413a1900e9b817cb85fbd662b017241753baff96
0xkhan <0xkhan@protonmail.com>
2023-05-14 22:53:08 +0200

    Add support for fetching data from Google Sheets

--- src/js/app/Model.js
+++ src/js/app/Model.js
@@ -1,14 +1,41 @@
-import { API_URL } from '../config';
-import { AJAX } from '../helpers';
+import { JSON_URL, SHEET_ID } from '../config';
+import { AJAX, fetchSheetData } from '../helpers';
 
 class Model {
 
-    state = {};
+    state = { data: {} };
+    #sheetBaseURL;
+    #sheetDataType;
+    #sheetLinks;
+    #sheetBio;
+    #linksURL;
+    #bioURL;
+
+    constructor() {
+        this.#sheetBaseURL = 'https://docs.google.com/spreadsheets/d/';
+        this.#sheetDataType = `/gviz/tq?tqx=out:json&headers=1`;
+        this.#sheetLinks = 'links';
+        this.#sheetBio = 'profile-data';
+
+        if (SHEET_ID !== null) {
+            this.#linksURL = this.#sheetBaseURL + SHEET_ID + this.#sheetDataType + '&sheet=' + encodeURIComponent(this.#sheetLinks);
+            this.#bioURL = this.#sheetBaseURL + SHEET_ID + this.#sheetDataType + '&sheet=' + encodeURIComponent(this.#sheetBio);
+        }
+    }
 
     async loadData() {
         try {
-            const data = await AJAX(API_URL);
-            this.state.data = data;
+            if (SHEET_ID !== null) {
+                const linksData = await fetchSheetData(this.#linksURL);
+                const bioData = await fetchSheetData(this.#bioURL);
+                this.state.data = linksData;
+                this.state.data.name = bioData.bio?.name;
+                this.state.data.profilePic = bioData.bio?.profilePic;
+                this.state.data.desc = bioData.bio?.description;
+            } else {
+                const data = await AJAX(JSON_URL);
+                this.state.data = data;
+            }
         } catch(err) {
             throw err;
         }
