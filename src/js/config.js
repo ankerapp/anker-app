261e5a2423cd1b56929ba20168d70d772fc5a956
0xkhan <0xkhan@protonmail.com>
2023-05-14 22:53:55 +0200

    Add support for fetching data from Google Sheets - Add new constant for Google Sheet ID

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
-export const API_URL = "https://ankerdata.netlify.app/dataPro.json";
+export const JSON_URL = null; // "https://ankerdata.netlify.app/dataPro.json"
+export const SHEET_ID = "1tdRaVMG9BSry5SpJCH09VkslR1Sc7tPYH4b5m-RJn9c"; // "1tdRaVMG9BSry5SpJCH09VkslR1Sc7tPYH4b5m-RJn9c"
 export const TIMEOUT_SEC = 10;
 export const THEME = "default-theme-pro";
